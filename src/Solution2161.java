4d8b9f14141f27484ac0125eb4d8ea9a02edebf0
CNA-YUN <zhangyun20210801@163.com>
2026-06-08 11:29:50 +0800

    问题2161;20260608

--- src/Solution2161.java
+++ src/Solution2161.java
@@ -1,5 +1,29 @@
 public class Solution2161 {
     public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int minpivot = 0, maxpivot = 0, equalpivot = 0;
 
+        for (int num : nums) {
+            if (num == pivot) {
+                equalpivot++;
+            } else if (num > pivot) {
+                maxpivot++;
+            } else {
+                minpivot++;
+            }
+        }
+        int left = 0, mid = minpivot, right = minpivot + equalpivot;
+        for (int num : nums) {
+            if (num == pivot) {
+                res[mid++] = num;
+            } else if (num > pivot) {
+                res[right++] = num;
+            } else {
+                res[left++] = num;
+            }
+        }
+
+        return res;
     }
 }
