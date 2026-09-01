6264389b7c709a3f81673cebbe09a882d0b30233
CNA-YUN <zhangyun20210801@163.com>
2026-09-01 19:16:02 +0800

    调用quickSort方法

--- src/Solution912.java
+++ src/Solution912.java
@@ -4,45 +4,9 @@ public class Solution912 {
     private final Random random = new Random();
 
     public int[] sortArray(int[] nums) {
+        Sort sort = new Sort();
         int n = nums.length;
-        quicksort(nums, 0, n - 1);
+        sort.quickSort(nums, 0, n - 1);
         return nums;
-    }
-
-    private void quicksort(int[] nums, int left, int right) {
-        if (left < right) {
-            int pivot = partition(nums, left, right);
-            quicksort(nums, left, pivot - 1);
-            quicksort(nums, pivot + 1, right);
-        }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private int partition(int[] nums, int left, int right) {
-        int p = left + random.nextInt(right - left + 1);
-        int pivot = nums[p];
-        swap(nums, p, left);
-        int i = left + 1;
-        int j = right;
-        while (true) {
-            while (i <= j && nums[j] >= pivot) {
-                j--;
-            }
-            while (i <= j && nums[i] <= pivot) {
-                i++;
-            }
-            if (i <= j) {
-                swap(nums, i, j);
-            } else {
-                break;
-            }
-        }
-        swap(nums, left, j);
-        return j;
     }
 }
