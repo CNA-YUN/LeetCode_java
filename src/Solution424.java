2b4e82ed7f0995bc90391625e91866810ca25524
CNA-YUN <zhangyun20210801@163.com>
2026-09-03 12:03:08 +0800

    滑动窗口；求符合条件的最长字串长度

--- src/Solution424.java
+++ src/Solution424.java
@@ -1,20 +1,17 @@
 public class Solution424 {
     public int characterReplacement(String s, int k) {
-        int sum = 0;
-        int mx = 0;
+        int mx = Integer.MIN_VALUE;
         int res = Integer.MIN_VALUE;
         int n = s.length();
         int[] map = new int[26];
         int left = 0;
         for (int i = 0; i < n; i++) {
-            sum++;
             mx = Math.max(mx, ++map[s.charAt(i) - 'A']);
-            while (sum - mx > k) {
-                sum--;
+            while (i - left + 1 - mx > k) {
                 mx = Math.max(mx, --map[s.charAt(left) - 'A']);
                 left++;
             }
-            res = Math.max(res, sum);
+            res = Math.max(res, i - left + 1);
         }
         return res;
     }
